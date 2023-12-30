31e1fe0993ba8fd3b9e4a19f71b7a0c3e2140162
Darshan Pai <darshanp20@yahoo.com>
2023-12-29 22:12:45 -0600

    Add more parse arguments

--- main_app.py
+++ main_app.py
@@ -8,6 +8,11 @@ parser = argparse.ArgumentParser(description="Implementation of Coffee Disease C
 ## Data parameters
 parser.add_argument("--data_path", type=str, default="/path/to/coffeedata",
                     help="path to dataset repository")
+build_or_load_group = parser.add_mutually_exclusive_group(required=True)
+build_or_load_group.add_argument("--build_data", type=str,
+                                 help="Build data per specification mentioned by string")
+build_or_load_group.add_argument("--load_data", type=str,
+                                 help="Load Train/Val/Test split from CSV")
 
 ## Optimization parameters
 parser.add_argument("--epochs", default=100, type=int,
@@ -20,9 +25,17 @@ parser.add_argument("--dump_path", type=str, default=".",
                     help="experiment dump path for checkpoints and log")
 parser.add_argument("--seed", type=int, default=31, help="set random seed")
 
+def printArguments(args):
+    print(args.data_path)
+    print(args.build_data)
+    print(args.load_data)
+    print(args.epochs)
+    print(args.batch_size)
+
 def main():
     global args
     args = parser.parse_args()
+    printArguments(args)
 
 
 if __name__ == '__main__':
